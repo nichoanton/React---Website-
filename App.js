7b5c6b026bd10685f1b9c9331023d498b20a8f75
Nicho Antony <58360342+nichoanton@users.noreply.github.com>
2022-05-02 14:54:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import { Route, Redirect } from "react-router-dom";
+
+import { Container } from "react-bootstrap";
+import Home from "./components/Pages/Home/1Home";
+import Pricing from "./components/Pages/Pricing/2Pricing";
+import MainHeader from "./components/layout/MainHeader";
+import Support from "./components/Pages/Support/3support";
+import TableContent from "./components/layout/TableContent";
+import Footer from "./components/layout/Footer";
+import ContactUs from "./components/Pages/Support/Contactus";
+
+function App() {
+  return (
+    <Container fluid="lg">
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Route path="/" exact>
+          <Redirect to="/Home"/>
+        </Route>
+        <Route path="/Home">
+          <Home />
+        </Route>
+        <Route path="/Pricing">
+          <Pricing />
+        </Route>
+        <Route path="/Support" exact>
+          <Support />
+        </Route>
+        <Route path="/Support/contact_us">
+          <ContactUs />
+        </Route>
+      </main>
+      <div>
+        <TableContent />
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+    </Container>
+  );
+}
+
+export default App;
